0aec343e1eda234c96d8d7b315bd9b7ac86d7625
Dan Hersam <dan@hersam.com>
2015-03-25 20:18:09 -0400

    Updated deploy script for readme
    It's now in markdown.

--- deploy.bat
+++ deploy.bat
@@ -8,5 +8,5 @@ IF EXIST %deploy% GOTO CONT
 mkdir %deploy%
 :CONT
 copy /Y %build%\SnapMD5.exe %deploy%\SnapMD5.exe
-copy /Y Readme.txt %deploy%\Readme.txt
+copy /Y Readme.md %deploy%\Readme.txt
 copy /Y license.txt %deploy%\license.txt
